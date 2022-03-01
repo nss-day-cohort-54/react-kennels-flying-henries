54bf596515c436727e144468ad9bba8e63723999
Randall Thomas <randallthomasmusic@gmail.com>
2022-03-01 09:01:13 -0800

    OxfordList.tsx comments

--- src/hooks/string/OxfordList.tsx
+++ src/hooks/string/OxfordList.tsx
@@ -1,19 +1,27 @@
+// export function using typescript 
+// declare data types
 export const OxfordList = (resources: Array<Object>, namespace: string) => {
-
+    // declare variable with data type and use split method on string 
     const propArray: Array<string> = namespace.split(".")
 
+    // function declares variable and state
     const display = (resource: Object) => {
+        // return reduced string array 
         return propArray.reduce(
+            // function declares var and data type to pass property string into data object
             (data: Object, property: string) => {
                 // @ts-ignore
                 return data[property]
             }, resource
         )
     }
-
+// return reduced resources 
     return resources.reduce(
+        // declare vars and data types
         (list: string, resource: Object, idx: number, resourceArray: Array<Object>) => {
+            // var holds value of display function with resource as argument passed in
             const output = display(resource)
+            // return list string and conditional output
             return `${list}${
                 (resourceArray.length > 1 && idx === resourceArray.length - 1)
                     ? `, and ${output}`
