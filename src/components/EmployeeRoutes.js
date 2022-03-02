3e21dbd412cfbdfab170794b609d02601b13d16a
Randall Thomas <randallthomasmusic@gmail.com>
2022-03-02 08:30:20 -0800

    comments 2nd day

--- src/components/EmployeeRoutes.js
+++ src/components/EmployeeRoutes.js
@@ -1,10 +1,11 @@
 import React from "react"
 import { Route } from "react-router-dom"
-
+// import all needed resources
 import Employee from "./employees/Employee"
 import EmployeeList from "./employees/EmployeeList"
 import EmployeeForm from "./employees/EmployeeForm"
 
+//since it just says export default, and no function name, it will be imported in another module - Kennel.js
 export default () => {
     return (
         <>
--- src/repositories/Fetch.js
+++ src/repositories/Fetch.js
@@ -1,9 +1,15 @@
+// this file has the responsibility to make all requests and posts from and to the API
+
+
+// function sets up a parameter for url and also sets the "method" param to "GET" for API call
+// also has "body" parameter set initially to null
 export const fetchIt = (url, method = "GET", body = null) => {
+    // sets up the options state for the API call
     let options = {
         "method": method,
         "headers": {}
     }
-
+    // checks if method equals "GET", if not it changes the API call method to "PUT"
     switch (method) {
         case "POST":
         case "PUT":
@@ -11,30 +17,34 @@ export const fetchIt = (url, method = "GET", body = null) => {
                 "Content-Type": "application/json"
             }
             break;
+            // sets up empty default?
         default:
             break;
     }
-
+    // checks if content is inside of body variable 
+    // and passes that data into options object 
     if (body !== null) {
         options.body = body
     }
-
+    // returns fetch request with url, options object and a .then 
     return fetch(url, options).then(r => r.json())
 }
 
+// request object holds API request functions and gets options object (made above) and url as data
 export const request = {
+    // initial API request for data to set up application state
     init(url) {
         this.options = {}
         this.options.headers = {}
         this.url = url
     },
-
+    
     get(url) {
         this.init(url)
         this.options.method = "GET"
         return this.send()
     },
-
+    
     post(url) {
         this.init(url)
         this.options.method = "POST"
@@ -57,7 +67,7 @@ export const request = {
         this.options.method = "DELETE"
         return this.send()
     },
-
+    // sends object to api that has body 
     withBody(body) {
         if (this.options.method === "POST" || this.options.method === "PUT") {
             this.options.body = JSON.stringify(body)
